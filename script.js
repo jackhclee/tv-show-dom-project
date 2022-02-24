f03a3778599407e50f3e497e068c348dd0b5d7e6
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2022-02-24 22:01:59 +0000

    update

--- script.js
+++ script.js
@@ -8,21 +8,22 @@ function setup() {
 
   rootElem.appendChild(makeSearchPanel());
 
-  const searchResultPanelElm = document.createElement('div');
-  searchResultPanelElm.setAttribute('id','searchPanel')
-
-  rootElem.appendChild(searchResultPanelElm);
-  makeSearchResultPanel(searchResultPanelElm, matchedEpisodes);
+  rootElem.appendChild(makeSearchResultPanel(matchedEpisodes));
   
 }
 
-function makeSearchResultPanel(searchResultPanelElm, episodes) {
+function makeSearchResultPanel(episodes) {
+  const searchResultPanelElm = document.createElement("div");
+  searchResultPanelElm.setAttribute("id","searchResultPanel");
   for (let episode of episodes) {
     searchResultPanelElm.append(makeEpisodeParaElm(episode));
   }
+  return searchResultPanelElm;
 }
 
-
+function removeSearchResultPanel() {
+  document.getElementById("searchResultPanel").remove();
+}
 
 function getEpisodeInfo(episodeNo) {
   const episodeInfoElem = document.getElementById("episodeInfo");
@@ -30,27 +31,40 @@ function getEpisodeInfo(episodeNo) {
 }
 
 function formatEpisodeCode(seasonNbr, episodeNbr) {
-  return 'S' + (seasonNbr < 10 ? '0' : '') + seasonNbr + 'E' + (episodeNbr < 10 ? '0' : '') + episodeNbr ;
+  return "S" + (seasonNbr < 10 ? "0" : "") + seasonNbr + "E" + (episodeNbr < 10 ? "0" : "") + episodeNbr ;
 }
 
-function makeSearchPanel() {
-  let searchPanelDivElm = document.createElement('div');
-  let searchInputElm = document.createElement('input');
+function displaySearchResult() {
 
-  searchInputElm.addEventListener('change', (evt)=> {
-    let searchWord = evt.target.value.toLocaleLowerCase.trim();
-    let matchedEpisodes = getAllGOTEpisodes().filter(
+  removeSearchResultPanel();
+  document.getElementById('root').appendChild(makeSearchResultPanel(matchedEpisodes));
+}
+function makeSearchPanel() {
+  let searchPanelDivElm = document.createElement("div");
+  let searchInputElm = document.createElement("input");
+
+  searchInputElm.addEventListener("change", (evt)=> {
+    let searchWord = evt.target.value.toLowerCase().trim();
+    console.log(searchWord)
+   
+    if (searchWord !== "") {
+      matchedEpisodes = getAllGOTEpisodes().filter(
       episode => 
-      (episode.name.toLocaleLowerCase.indexOf(searchWord) >=0 ||
-      episode.summary.toLocaleLowerCase.indexOf(searchWord) >=0)
+      (episode.name.toLowerCase().indexOf(searchWord) >=0 ||
+      episode.summary.toLowerCase().indexOf(searchWord) >=0)
       
-      )
-    
-    evt.target.value = '';
-  })
+      );
+    } else {
+      matchedEpisodes = getAllGOTEpisodes();
+    }
+      console.table(matchedEpisodes);
+      updateSearchResultStat(matchedEpisodes.length, getAllGOTEpisodes().length);
+      displaySearchResult();
+    evt.target.value = "";
+  });
 
   let allGOTEpisodes = getAllGOTEpisodes();
-  let SearchResultSpan = makeSearchResultSpan(allGOTEpisodes.length, allGOTEpisodes.length);
+  let SearchResultSpan = makeSearchResultStatSpan(allGOTEpisodes.length, allGOTEpisodes.length);
   
   searchPanelDivElm.appendChild(searchInputElm);
   searchPanelDivElm.appendChild(SearchResultSpan);
@@ -58,32 +72,38 @@ function makeSearchPanel() {
   return searchPanelDivElm;
 }
 
-function makeSearchResultSpan(episodeFound, totalEpisode) {
-  let searchResultSpanElm = document.createElement('span');
-  searchResultSpanElm.innerHTML = `Displaying ${episodeFound}/${totalEpisode} episodes`;
+function updateSearchResultStat(episodeFound, totalEpisode) {
+  document.getElementById('searchResultStat').innerHTML
+  = `Displaying ${episodeFound}/${totalEpisode} episodes`;
+}
 
-  return searchResultSpanElm;
+function makeSearchResultStatSpan(episodeFound, totalEpisode) {
+  let searchResultStatSpanElm = document.createElement("span");
+  searchResultStatSpanElm.setAttribute('id','searchResultStat')
+  searchResultStatSpanElm.innerHTML = `Displaying ${episodeFound}/${totalEpisode} episodes`;
+
+  return searchResultStatSpanElm;
 }
 
 function makeEpisodeParaElm(episode) {
-  let episodeParaElm = document.createElement('p');
+  let episodeParaElm = document.createElement("p");
 
-  let episodeNumberSpanElm = document.createElement('span');
-  episodeNumberSpanElm.innerHTML = formatEpisodeCode(episode.season, episode.number)
+  let episodeNumberSpanElm = document.createElement("span");
+  episodeNumberSpanElm.innerHTML = formatEpisodeCode(episode.season, episode.number);
 
-  let titleSpanElm = document.createElement('span');
+  let titleSpanElm = document.createElement("span");
   titleSpanElm.innerHTML = episode.name;
 
-  let episodeImgElm = document.createElement('img');
+  let episodeImgElm = document.createElement("img");
   episodeImgElm.src = episode.image.medium;
 
-  let synopsisElm = document.createElement('span');
-  synopsisElm.innerHTML = episode.summary
+  let synopsisElm = document.createElement("span");
+  synopsisElm.innerHTML = episode.summary;
 
-  episodeParaElm.appendChild(episodeNumberSpanElm)
-  episodeParaElm.appendChild(titleSpanElm)
-  episodeParaElm.appendChild(episodeImgElm)
-  episodeParaElm.appendChild(synopsisElm)
+  episodeParaElm.appendChild(episodeNumberSpanElm);
+  episodeParaElm.appendChild(titleSpanElm);
+  episodeParaElm.appendChild(episodeImgElm);
+  episodeParaElm.appendChild(synopsisElm);
 
   return episodeParaElm;
 }
