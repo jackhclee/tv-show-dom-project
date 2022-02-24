b0b643dc4c9627e97b240748a402c842fed97901
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2022-02-24 13:30:07 +0000

    update

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function setup() {
   // const allEpisodes = getAllEpisodes();
   const allEpisodes = getAllGOTEpisodes();
   makePageForEpisodes(allEpisodes);
+
+  const rootElem = document.getElementById("root");
+
+  for (let episode in allEpisodes) {
+    rootElem.append(makeEpisodeParaElm(episode))
+  }
 }
 
 function makePageForEpisodes(episodeList) {
@@ -15,4 +21,29 @@ function getEpisodeInfo(episodeNo) {
   return episodeInfoElem.textContent = getAllGOTEpisodes()[episodeNo]["name"];
 }
 
+function formatEpisodeCode(seasonNbr, episodeNbr) {
+  return + 'S' + (seasonNbr < 10 ? '0' : '') + 'E' + (episodeNbr < 10 ? '0' : '') ;
+}
+function makeEpisodeParaElm(episode) {
+  let episodeParaElm = document.createElement('p');
+
+  let episodeNumberSpanElm = document.createElement('span');
+  episodeNumberSpanElm.innerHTML = formatEpisodeCode(episode.season, episode.number)
+
+  let titleSpanElm = document.createElement('img');
+  titleSpanElm.innerHTML = episode.name;
+
+  let episodeImgElm = document.createElement('span');
+  episodeImgElm.src = episode.image.medium;
+
+  let synopsisElm = document.createElement('span');
+  synopsisElm.innerHTML = episode.summary
+
+  episodeParaElm.appendChild(episodeNumberSpanElm)
+  episodeParaElm.appendChild(titleSpanElm)
+  episodeParaElm.appendChild(episodeImgElm)
+  episodeParaElm.appendChild(synopsisElm)
+
+  return episodeParaElm;
+}
 window.onload = setup;
